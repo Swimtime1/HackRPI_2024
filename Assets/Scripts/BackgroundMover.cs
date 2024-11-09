aa7ec882529805b01e8238bec625230873b4498f
Swimtime1 <124410615+Swimtime1@users.noreply.github.com>
2024-11-09 12:12:53 -0500

    BackgroundMover
    Created a Prefab for the background, and changed the name of the script to control the infinite running

--- Assets/Scripts/BackgroundMover.cs
+++ Assets/Scripts/BackgroundMover.cs
@@ -2,7 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class BackgroundManager : MonoBehaviour
+public class BackgroundMover : MonoBehaviour
 {
     // Start is called before the first frame update
     void Start()
