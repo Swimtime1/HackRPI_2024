4707ebc0f29bdac41dfa79c62e866487cad37bca
Swimtime1 <124410615+Swimtime1@users.noreply.github.com>
2024-11-09 13:21:34 -0500

    Update GameManager.cs

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -22,7 +22,7 @@ public class GameManager : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        gameStarted = false;
+        gameStarted = true;
         gameActive = false;
         
         /* OpenStart(); */
