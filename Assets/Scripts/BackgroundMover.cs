91a21eadb6c81e840bc4803a20ab40690ab29ac7
Swimtime1 <124410615+Swimtime1@users.noreply.github.com>
2024-11-09 12:33:17 -0500

    Background
    Continued working on the background mover

--- Assets/Scripts/BackgroundMover.cs
+++ Assets/Scripts/BackgroundMover.cs
@@ -4,15 +4,29 @@ using UnityEngine;
 
 public class BackgroundMover : MonoBehaviour
 {
+    #region Variables
+
+    [Header("Positions")]
+    [SerializeField] private Vector2 gameStartPos;
+    [SerializeField] private Vector2 startPos;
+    [SerializeField] private Vector2 endPos;
+
+    #endregion Variables
+    
     // Start is called before the first frame update
     void Start()
     {
-        
+        // set the current location to gameStartPos
+        transform.position = gameStartPos;
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        // Resets the position to the far right if the far left was reached
+        if(transform.position.x <= endPos.x) { transform.Translate(startPos); }
+
+        // Moves this GameObject to the left
+        else { transform.Translate(startPos); }
     }
 }
