7cc08ff90771d8b508663a0db58566b2c2a5cdf9
GoatHero <72709281+GoatHero@users.noreply.github.com>
2024-11-10 05:05:17 -0500

    intersection pain

--- Assets/Scripts/Signs/ActiveSigns/Intersection.cs
+++ Assets/Scripts/Signs/ActiveSigns/Intersection.cs
@@ -1,3 +1,4 @@
+using System.Collections.Generic;
 using UnityEngine;
 
 public class Intersection : MonoBehaviour {
@@ -7,10 +8,11 @@ public class Intersection : MonoBehaviour {
     public GameObject npcCar;
     private bool npcCarGoesFirst;
     private bool playerCanPass = false;
+    private BoxCollider2D boxCollider;
 
     // Start is called before the first frame update
     void Start() {
-
+        boxCollider = GetComponent<BoxCollider2D>();
         npcCarGoesFirst = Random.Range(0, 1) == 0;
         if(npcCarGoesFirst) {
             npcCar.GetComponent<Animation>().Play("topDownNpcCar");
@@ -23,6 +25,9 @@ public class Intersection : MonoBehaviour {
 
     // Update is called once per frame
     void Update() {
+        if(boxCollider.OverlapCollider(new ContactFilter2D().NoFilter(), new List<Collider2D>()) == 1) {
+            Debug.Log("TRIGGER");
+        }
         if(GameManager.gameActive) {
             playerCar.transform.Translate(1 * Time.deltaTime, 0, 0);
         }
