a65354e5eb720aaa584509de0cf9fcf7b1eca639
GoatHero <72709281+GoatHero@users.noreply.github.com>
2024-11-10 02:04:36 -0500

    car animation

--- Assets/Scripts/Signs/ActiveSigns/Intersection.cs
+++ Assets/Scripts/Signs/ActiveSigns/Intersection.cs
@@ -4,13 +4,21 @@ public class Intersection : MonoBehaviour {
 
     public GameObject playerCar;
 
+    public GameObject npcCar;
+    private bool npcCarGoesFirst;
+
     // Start is called before the first frame update
     void Start() {
 
+        npcCarGoesFirst = (Random.Range(0, 1) == 0) ? true : false;
+        if(npcCarGoesFirst) {
+        }
     }
 
     // Update is called once per frame
     void Update() {
-
+        if(GameManager.gameActive) {
+            npcCar.transform.Translate(10, 0, 0);
+        }
     }
 }
